37a8ad9e8f19faac23e8e9939e13414a17e716f9
Moritz Diedrich <mrtz.ddrch@gmx.de>
2022-03-19 11:34:47 +0100

    cannot figure this error out

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const time = document.querySelector('#time');
 const score = document.querySelector('#score');
 
 let result = 0;
+let hitPosition;
 
 // function: randomly assign #mole to one of the .square
 // assign an image along with the class
@@ -16,6 +17,26 @@ function randomSquare(){
     squares.forEach(square => {
         square.classList.remove('mole');
     });
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)];
+    randomSquare.classList.add('mole');
+    hitPosition = randomSquare.id;
 }
 
+function moveMole(){
+    let timerId = null;
+    timerId = setInterval(randomSquare, 500);
+}
 
+squares.forEach(square => {
+    square.addEventListener('mousedown' () => {
+        if(square.id == hitPosition){
+            result++;
+            score.textContent = result;
+            hitPosition = null;
+        }
+    })
+});
+
+
+moveMole();
